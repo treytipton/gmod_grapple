214c562c985666cc8139f2342e148ecbd2348356
treytipton <treytipto@outlook.com>
2024-10-02 07:37:53 -0500

    Added init.lua and addon.json
    Adds basic information and initializes addon basics.

---
+++ lua/weapons/weapon_grapplehook/init.lua
@@ -0,0 +1,25 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function SWEP:Initialize()
+    self:SetHoldType("crossbow")
+end
+
+function SWEP:PrimaryAttack()
+    if SERVER then
+        local owner = self:GetOwner()
+        local eyeTrace = owner:GetEyeTrace()    //!< Find where player is looking.
+
+        if eyeTrace.Hit then
+            local hookPos = eyeTrace.HisPos
+
+            local distance = (hookPos - owner:GetPos()):Length()
+            if distance > 2000 then return end-- Limit grapple hook hit range.
+        
+            local phys = owner:GetPhysicsObject()
+            if IsValid(phys) then
+                phys:SetVelocity(hookPos - owner:getPos():GetNormalized() * 300) -- Pull the player toward the hook point
+            end
+        end
+    end
